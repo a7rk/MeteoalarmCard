1b82650cd25821e4a10f478a97f871a46344e5cc
a7rk <ako.github@kortstoy.com>
2026-08-18 10:43:51 +0200

    chore: clean up code

--- src/editor.ts
+++ src/editor.ts
@@ -13,7 +13,6 @@ import {
 	MeteoalarmIntegrationEntityType,
 	MeteoalarmScalingMode,
 	WarningRule,
-	MeteoalarmCardStyle,
 } from './types';
 
 @customElement('meteoalarm-card-editor')
@@ -39,14 +38,6 @@ export class MeteoalarmCardCardEditor extends LitElement implements LovelaceCard
 			this.formData = this.computeFormData(this.integration);
 			this.warning = this.computeWarnings(this.integration);
 		}
-	}
-
-	get _card_style(): string {
-		return this._config?.card_style || 'card';
-	}
-
-	get _color_mode(): string {
-		return this._config?.color_mode || 'background';
 	}
 
 	get _card_style(): string {
